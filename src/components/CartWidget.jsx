f5eac83fd527a4f2ca5dc1b35c4c6e6622c3f84a
Marcos Lopez <marcosleonellopez@gmail.com>
2022-12-26 20:00:41 -0300

    PreEntrega1_Lopez

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUtensils } from '@fortawesome/free-solid-svg-icons'
-
-
+import ItemListContainer from "./ItemListContainer";
 
 const element = <FontAwesomeIcon icon={faUtensils} />
 
-
-
 const CartWidget = () => {
-  return (
-    <div  className="Carrito">
-   <div>
-    {element}
-    </div>
-    <div>
-        5
-    </div>
-</div> );
+    return (
+        <div className="Carrito">
+            <div>{element}</div>
+            <ItemListContainer greeting={10} />
+        </div>);
 };
 
 export default CartWidget;
